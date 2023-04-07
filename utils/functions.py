341f29bfa999882ddf4d0688d65b502cd4006798
sabilmakbar <maulana.1998@yahoo.co.id>
2023-04-07 14:00:41 +0000

    Fixing import in fns

--- utils/functions.py
+++ utils/functions.py
@@ -11,7 +11,8 @@ from transformers import MBartTokenizer, MBartConfig, MBartForConditionalGenerat
 from transformers import T5Config, T5Tokenizer, T5ForConditionalGeneration
 from transformers import MT5ForConditionalGeneration
 from transformers import GPT2Tokenizer, GPT2Config, GPT2LMHeadModel
-from indobenchmark import IndoNLGTokenizer
+# from indobenchmark import IndoNLGTokenizer
+from modules.tokenization_indonlg import IndoNLGTokenizer
 from modules.tokenization_mbart52 import MBart52Tokenizer
 
 import json
