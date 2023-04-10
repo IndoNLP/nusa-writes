36b67600f87953e7f1d7a8d765d27c1d1aea36e4
sabilmakbar <maulana.1998@yahoo.co.id>
2023-04-10 15:02:37 +0000

    Moving args_helper for dataset and task choice to appending fn (since there's only 1 valid value for each)

--- main_generation.py
+++ main_generation.py
@@ -176,9 +176,10 @@ if __name__ == "__main__":
     args = append_generation_model_args(args)
 
     ## Helper 1: Create output directory
-    def create_output_directory(model_dir, task, dataset, model_checkpoint, seed, num_sample, force):
-        output_dir = '{}/{}/{}/{}_{}_{}'.format(
+    def create_output_directory(model_dir, dataset_name, task, dataset, model_checkpoint, seed, num_sample, force):
+        output_dir = '{}/{}/{}/{}/{}_{}_{}'.format(
             model_dir,
+            dataset_name,
             task,
             dataset,
             model_checkpoint.replace('/','-'),
@@ -199,6 +200,7 @@ if __name__ == "__main__":
     # Specify output dir
     output_dir = create_output_directory(
         args["model_dir"],
+        args["dataset_name"],
         args["task"],
         args["lang"],
         args['model_type'].replace('/','-'),
