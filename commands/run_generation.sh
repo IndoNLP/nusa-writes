3c581301e4c90647f8d22f95933bb75baa4a509d
sabilmakbar <maulana.1998@yahoo.co.id>
2023-04-10 16:04:44 +0000

    Bugfixing on run_generation.sh (already works)

--- commands/run_generation.sh
+++ commands/run_generation.sh
@@ -1,18 +1,25 @@
-# hyperparams to execute script, pls change it appropriately for ur needs
+#!/bin/bash
+
+# hyperparams to execute script, pls change it appropriately for your needs
 num_sample=4
 train_batch_size=4
+valid_batch_size=4
 test_batch_size=4
 seed=0
-n_epochs=5
+n_epochs=2
 lr=10e-5
+device=cpu
+
+echo "Current directory: $PWD"
 
 ## -- NUSA_KALIMAT -- ##
 # mt w/o models checkpoint (for indo-bart and indo-gpt)
-model_choices = (indo-bart indo-gpt2)
+model_choices=(indo-bart indo-gpt2)
 for lang in abs bew bhp btk jav mad mak min mui rej sun; do
-    for ((i = 0; i < ${model_choices[@]}; ++i)); do
-        python main_generation.py --lang $lang --model_type ${model_choices[$i]} --n_epochs $n_epochs --lr $lr --train_batch_size $train_batch_size --eval_batch_size $eval_batch_size --seed $seed --num_sample $num_sample --force
-        # CUDA_VISIBLE_DEVICES=0 python main_generation.py --lang $lang --model_type ${model_choices[$i]} --n_epochs $n_epochs --lr $lr --train_batch_size $train_batch_size --eval_batch_size $eval_batch_size --seed $seed --num_sample $num_sample &
+    for ((i = 0; i < ${#model_choices[@]}; ++i)); do
+        echo "Executing MT task on lang ${lang} and model choice ${model_choices[$i]}"
+        python main_generation.py --lang $lang --model_type ${model_choices[$i]} --device $device --n_epochs $n_epochs --lr $lr --train_batch_size $train_batch_size --valid_batch_size $valid_batch_size --test_batch_size $test_batch_size --seed $seed --num_sample $num_sample --force
+        # CUDA_VISIBLE_DEVICES=0 python main_generation.py --lang $lang --model_type ${model_choices[$i]} --device $device --n_epochs $n_epochs --lr $lr --train_batch_size $train_batch_size --valid_batch_size $valid_batch_size --test_batch_size $test_batch_size --seed $seed --num_sample $num_sample &
     done
     wait
     rm -r save/nusa_kalimat/mt/$lang/*/checkpoint*
@@ -23,9 +30,10 @@ done
 model_checkpoints=(facebook/mbart-large-50-one-to-many-mmt google/mt5-base)
 model_choices_w_checkpoint=(baseline-mbart baseline-mt5)
 for lang in abs bew bhp btk jav mad mak min mui rej sun; do
-    for ((i = 0; i < ${model_choices_w_checkpoint[@]}; ++i)); do
-        python main_generation.py --lang $lang --model_type ${model_choices_w_checkpoint[$i]} --model_checkpoint ${model_checkpoints[$i]} --n_epochs $n_epochs --lr $lr --train_batch_size $train_batch_size --eval_batch_size $eval_batch_size --seed $seed --num_sample $num_sample --force
-        # CUDA_VISIBLE_DEVICES=0 python main_generation.py --lang $lang --model_type ${model_choices_w_checkpoint[$i]} --model_checkpoint ${model_checkpoints[$i]} --n_epochs $n_epochs --lr $lr --train_batch_size $train_batch_size --eval_batch_size $eval_batch_size --seed $seed --num_sample $num_sample --force
+    for ((i = 0; i < ${#model_choices_w_checkpoint[@]}; ++i)); do
+        echo "Executing MT task on lang ${lang} and model choice ${model_choices[$i]}"
+        python main_generation.py --lang $lang --model_type ${model_choices_w_checkpoint[$i]} --model_checkpoint ${model_checkpoints[$i]} --device $device --n_epochs $n_epochs --lr $lr --train_batch_size $train_batch_size --valid_batch_size $valid_batch_size --test_batch_size $test_batch_size --seed $seed --num_sample $num_sample --force
+        # CUDA_VISIBLE_DEVICES=0 python main_generation.py --lang $lang --model_type ${model_choices_w_checkpoint[$i]} --model_checkpoint ${model_checkpoints[$i]} --device $device --n_epochs $n_epochs --lr $lr --train_batch_size $train_batch_size --valid_batch_size $valid_batch_size --test_batch_size $test_batch_size --seed $seed --num_sample $num_sample --force
     done
     wait
     rm -r save/nusa_kalimat/mt/$lang/*/checkpoint*
