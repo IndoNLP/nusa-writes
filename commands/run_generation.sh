2cf74840ae005c8a470cca03870dd276cf137bdd
Jhonson Lee <jhonsonlee10@gmail.com>
2023-04-19 09:48:16 +0000

    add command for classical baseline

--- commands/run_generation.sh
+++ commands/run_generation.sh
@@ -38,3 +38,14 @@ for lang in abs bew bhp btk jav mad mak min mui rej sun; do
     wait
     rm -r save/nusa_kalimat/mt/$lang/*/checkpoint*
 done
+
+# mt with classical methods (for copy, word-substitution, and pbsmt)
+model_choices=(copy word-substitution) # add pbsmt if already implemented
+for lang in abs bew bhp btk jav mad mak min mui rej sun; do
+    for ((i = 0; i < ${#model_choices[@]}; ++i)); do
+        echo "Executing MT task on lang ${lang} and model choice ${model_choices[$i]}"
+        python main_generation.py --lang $lang --model_type ${model_choices[$i]} --seed $seed --num_sample $num_sample --force
+    done
+    wait
+    rm -r save/nusa_kalimat/mt/$lang/*/checkpoint*
+done
