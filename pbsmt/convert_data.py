693b46d94656f4a45229762f260a2a9a02f697dd
Tjeng Wawan Cenggoro <wawancenggoro@gmail.com>
2023-04-20 15:35:17 +0700

    add dataset conversion to moses format

---
+++ pbsmt/convert_data.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import os
+from tqdm import tqdm
+
+for split_set in ['train', 'valid', 'test']:
+    print(split_set)
+    for lang in tqdm(['abs','btk','bew','bhp','jav','mad','mak','min','mui','rej','sun']):
+        path = f'data/{lang}'
+        isExist = os.path.exists(path)
+        if not isExist:
+            os.makedirs(path)
+        data_path = f'../data/nusa_kalimat-mt-{lang}-test.csv'
+        df = pd.read_csv(data_path)
+        df['ind_text'].to_csv(f'data/{lang}/{split_set}.ind', header=None, index=False)
+        df['tgt_text'].to_csv(f'data/{lang}/{split_set}.tgt', header=None, index=False)
